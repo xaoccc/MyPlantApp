68e3e7e21b2f870a46cde7c859635eb3dce5009c
xaoccc <xaocccc@gmail.com>
2024-02-21 17:36:33 +0200

    update

--- MyPlantApp/profiles/urls.py
+++ MyPlantApp/profiles/urls.py
@@ -3,4 +3,7 @@ from MyPlantApp.profiles import views
 
 urlpatterns = [
     path('create', views.ProfileCreateView.as_view(), name='profile-create'),
+    path('details', views.ProfileDetailView.as_view(), name='profile-details'),
+    path('edit', views.ProfileEditView.as_view(), name='profile-edit'),
+    path('delete', views.ProfileDeleteView.as_view(), name='profile-delete'),
 ]
--- MyPlantApp/profiles/views.py
+++ MyPlantApp/profiles/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
 from MyPlantApp.profiles.models import Profile
+from MyPlantApp.plant.models import Plant
+
+class GetProfileMixin:
+    def get_object(self, queryset=None):
+        return Profile.objects.first()
+
+class PlantDeleteView(DeleteView):
+    model = Plant
+
+    def delete(self, request, *args, **kwargs):
+        Plant.objects.all().delete()
+        return super().delete(request, *args, **kwargs)
 
 
 class ProfileCreateView(CreateView):
@@ -10,4 +21,43 @@ class ProfileCreateView(CreateView):
     fields = ['username', 'first_name', 'last_name']
     def get_success_url(self):
         return reverse('plant-catalogue')
+
+
+class ProfileDetailView(GetProfileMixin, DetailView):
+    model = Profile
+    template_name = 'profile-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['plants'] = Plant.objects.all()
+        context['plants_count'] = ""
+        for star in Plant.objects.all():
+            context['plants_count'] += "1"
+            if len(context['plants_count']) > 2:
+                break
+
+        return context
+
+
+class ProfileEditView(GetProfileMixin, UpdateView):
+    model = Profile
+    fields = "__all__"
+    template_name = 'edit-profile.html'
+    success_url = reverse_lazy("profile-details")
+
+
+class ProfileDeleteView(GetProfileMixin, DeleteView):
+    model = Profile
+    template_name = 'delete-profile.html'
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        Plant.objects.all().delete()
+        return super().form_valid(form)
+
+
+
+
+
+
 
